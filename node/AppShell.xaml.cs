36570d4482f1df2eecb6961d27191cbcd83405a9
Fernand0Silva <luizfernando3995@gmail.com>
2024-02-26 21:48:44 -0300

    first commit

---
+++ node/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace node
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ node/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Nodes
+{
+
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string nome = " "; // declarando uma variavel
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnNameclicked(object sender, EventArgs e)
+        {
+            nome = NameEntry.Text;
+            //Interpolação
+            GreetingLabel.Text = $"hello,{nome}";
+
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+        }
+
+
+    }
+
+}
